f6d711e6d5e946fea654f1bd5447ed66f40bb777
NDKhoa1307 <126633757+NDKhoa1307@users.noreply.github.com>
2023-05-19 11:36:32 +0700

    Write all the codes to file Perfect Palindrome

--- Perfect_Palindrome.cpp
+++ Perfect_Palindrome.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+#include<string.h>
 
+using namespace std;
+
+inline bool is_palindrome(string s){
+    for(int i = 0; i < (s.length() / 2) - (s.length() & 1); ++i){
+        if(s[i] != s[s.length() - i - 1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+inline void solve(string s, int ti){
+    if(is_palindrome(s)){
+	    cout<<"YES\n";
+	}
+	else{
+	    cout<<"NO\n";
+	}
+}
+
+int main(){
+    string s;
+    int T;
+    long long n;
+    
+    cin >> T;
+    cin.ignore();
+    while (T) 
+    {
+        getline(cin, s);
+        cin >> n;
+        cin.ignore();
+        solve(s, n);
+        --T;
+    }
+	return 0;
+}
